7dfe23ffef14ffc53c98cdc182593df3cb70d29c
Stas74 <splotnikov74@gmail.com>
2022-03-08 16:33:23 -0700

    Add Exercise 6: One-to-many association

--- exercises/exercise_6.rb
+++ exercises/exercise_6.rb
@@ -9,3 +9,13 @@ puts "Exercise 6"
 puts "----------"
 
 # Your code goes here ...
+class Employee < ActiveRecord::Base
+  belongs_to :store  
+end
+
+@store1.employees.create(first_name: "Khurram", last_name: "Virani", hourly_rate: 60)
+@store2.employees.create(first_name: "Alan", last_name: "Bell", hourly_rate: 45)
+@store1.employees.create(first_name: "Rick", last_name: "Kurt", hourly_rate: 51)
+@store2.employees.create(first_name: "Anny", last_name: "Mills", hourly_rate: 58)
+@store1.employees.create(first_name: "Mick", last_name: "Moon", hourly_rate: 42)
+@store2.employees.create(first_name: "Kathy", last_name: "Blim", hourly_rate: 56)
